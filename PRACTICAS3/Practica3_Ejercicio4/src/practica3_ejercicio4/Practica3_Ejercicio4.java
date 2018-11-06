137ddbfe6aa4ad6e20978d0f4fa05c99b4dd6cad
Ángel <angelhml1997@gmail.com>
2018-11-06 11:46:35 +0100

    ejercicios finales + resultados de niever

--- PRACTICAS3/Practica3_Ejercicio4/src/practica3_ejercicio4/Practica3_Ejercicio4.java
+++ PRACTICAS3/Practica3_Ejercicio4/src/practica3_ejercicio4/Practica3_Ejercicio4.java
@@ -27,39 +27,49 @@ public class Practica3_Ejercicio4 {
     */
     public static void main(String[] args) {
         
-        //FALLA QUE NO SE REPITE ---------------------
+        //FALTARIA AÑADIR LOS TRY, CATCH Y AÑADIR OBLIGACIONES COMO SOLO NUMERO O SOLO LETRA
         String alumno;
-        int nota1;
-        int nota2;
-        int nota3;
-        int nota4;
-        int nota5;
-        int nota6;
+        int nota1 = 0;
+        int nota2 = 0;
+        int nota3 = 0;
+        int nota4 = 0;
+        int nota5 = 0;
+        int nota6 = 0;
         int mediaAlumno;
         int contador = 0;
+        int sumaMedias = 0;
+        int notaMediaFinal;
         //alumno y notas
         do
         {
         alumno = JOptionPane.showInputDialog("Intruduzca el nombre de los alumnos cuando termine escriba FIN");
-        nota1 = Integer.parseInt(JOptionPane.showInputDialog("Nota de primera asignatura"));
-        nota2 = Integer.parseInt(JOptionPane.showInputDialog("Nota de segunda asignatura"));
-        nota3 = Integer.parseInt(JOptionPane.showInputDialog("Nota de tercera asignatura"));
-        nota4 = Integer.parseInt(JOptionPane.showInputDialog("Nota de cuarta asignatura"));
-        nota5 = Integer.parseInt(JOptionPane.showInputDialog("Nota de quinta asignatura"));
-        nota6 = Integer.parseInt(JOptionPane.showInputDialog("Nota de sexta asignatura"));
-        
-        //media de alumno
-        mediaAlumno = (nota1+nota2+nota3+nota4+nota5+nota6) / 6;
-        JOptionPane.showMessageDialog(null,"La nota media de " + alumno + " es " + mediaAlumno);
-        contador = contador + 1;
+            if (!alumno.equals("FIN")) 
+            {
+                nota1 = Integer.parseInt(JOptionPane.showInputDialog("Nota de primera asignatura"));
+                nota2 = Integer.parseInt(JOptionPane.showInputDialog("Nota de segunda asignatura"));
+                nota3 = Integer.parseInt(JOptionPane.showInputDialog("Nota de tercera asignatura"));
+                nota4 = Integer.parseInt(JOptionPane.showInputDialog("Nota de cuarta asignatura"));
+                nota5 = Integer.parseInt(JOptionPane.showInputDialog("Nota de quinta asignatura"));
+                nota6 = Integer.parseInt(JOptionPane.showInputDialog("Nota de sexta asignatura"));
+                //media de alumno
+                mediaAlumno = (nota1+nota2+nota3+nota4+nota5+nota6) / 6;
+                JOptionPane.showMessageDialog(null,"La nota media de " + alumno + " es " + mediaAlumno);
+                contador = contador + 1;
+                sumaMedias = sumaMedias + mediaAlumno;
+             }
         }
-        while(alumno.equals("FIN"));
-        
+        while(!alumno.equals("FIN"));
         //contador de alumnos para hacer media de conjunto de alumnos puestos
-        int sumaMedias = 0;
-        sumaMedias = sumaMedias + mediaAlumno;
-        int notaMediaFinal = sumaMedias / contador;
-        JOptionPane.showMessageDialog(null,"La media general es " + notaMediaFinal);
+        if (sumaMedias != 0) {
+            notaMediaFinal = sumaMedias / contador;
+            JOptionPane.showMessageDialog(null,"La media general es " + notaMediaFinal);
+        }
+        else
+        {
+            JOptionPane.showMessageDialog(null,"No hay ninguna media");
+        }
+        
+        
     }
     
 }
