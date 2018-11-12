29210f0a172918355201ea4bb9d665b5857db56a
Ángel <angelhml1997@gmail.com>
2018-11-12 12:58:05 +0100

    cambios

---
+++ TEMA 3/PRACTICAS3/Practica3_Ejercicio5/src/practica3_ejercicio5/LongitudNoValida.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica3_ejercicio5;
+
+/**
+ *
+ * @author 1gdaw06
+ */
+public class LongitudNoValida extends Exception{
+    
+}
+
