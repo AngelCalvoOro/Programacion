6bfe7af01e90ecfa0a8621b2c11ade13f2cb423b
Ángel <angelhml1997@gmail.com>
2018-12-10 14:10:57 +0100

    actualizacion

---
+++ TEMA 6/Ejercicios/Practica1/Practica1_Ejercicio3/src/practica1_ejercicio3/Practica1_Ejercicio3.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica1_ejercicio3;
+
+/**
+ *
+ * @author 1gdaw06
+ */
+public class Practica1_Ejercicio3 {
+
+    /**
+    3. Define una clase Persona con los siguientes datos: 
+    nombre,
+    dNacimiento (dıa de nacimiento),
+    mNacimiento (mes de nacimiento),
+    aNacimiento (año de nacimiento),
+    direccion, codigo postal y ciudad.
+    
+    Crea varios objetos de tipo Persona y a traves de un programa muestra
+    los siguientes datos:
+        -Nombre de la persona de mayor edad.
+        -Nombre de las personas que viven en Elche.
+        -Numero de personas mayores de edad.
+    
+    Hay que comprobar que todos los campos contienen informacion y
+    que es logica.
+    */
+    public static void main(String[] args) {
+        
+    }
+    
+}
