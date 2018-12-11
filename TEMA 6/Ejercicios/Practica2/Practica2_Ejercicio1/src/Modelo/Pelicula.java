58be528ff85f4dab0eaea8f39a6ddbb99819298e
Ángel <angelhml1997@gmail.com>
2018-12-11 10:59:32 +0100

    fin practica 1 inicio practica 2
    hacer para el viernes

---
+++ TEMA 6/Ejercicios/Practica2/Practica2_Ejercicio1/src/Modelo/Pelicula.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author 1gdaw06
+ */
+public class Pelicula {
+    private String titulo;
+    private String año;
+    private String duracion;
+    private String tipo;
+    
+    public Pelicula(String titulo,String año,String duracion,String tipo){
+        
+    }
+}
