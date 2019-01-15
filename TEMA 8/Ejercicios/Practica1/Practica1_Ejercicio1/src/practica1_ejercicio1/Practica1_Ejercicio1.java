a5cb8a7ad69896f2af6ff0d5c65d64bc548360a9
Ángel <angelhml1997@gmail.com>
2019-01-15 10:57:08 +0100

    practica1 sin terminar

--- TEMA 8/Ejercicios/Practica1/Practica1_Ejercicio1/src/practica1_ejercicio1/Practica1_Ejercicio1.java
+++ TEMA 8/Ejercicios/Practica1/Practica1_Ejercicio1/src/practica1_ejercicio1/Practica1_Ejercicio1.java
@@ -2,11 +2,20 @@
 package practica1_ejercicio1;
 
 
+
 public class Practica1_Ejercicio1 {
+
+    private static Ventana1 v;
 
     
     public static void main(String[] args) {
-        
+        //crear y visualizar ventana
+        v = new Ventana1();
+        v.setVisible(true);
+    }
+    public static void salir(){
+        v.dispose();
+        System.exit(0);
     }
     
 }
---
+++ TEMA 8/Ejercicios/Practica1/Practica1_Ejercicio2/src/practica1_ejercicio2/Practica1_Ejercicio2.java
@@ -0,0 +1,19 @@
+
+package practica1_ejercicio2;
+
+import Ventana.Ventana_Alta;
+
+
+public class Practica1_Ejercicio2 {
+
+    private static Ventana_Alta vLista;
+
+    
+    public static void main(String[] args) {
+        //crear y visualizacion de ventana_lista
+        vLista = new Ventana_Alta();
+        vLista.setVisible(true);
+    }
+    
+    
+}
