cf495c8fdbbfc8bb9177e95ec4a543d2d98d1e8c
Ángel <angelhml1997@gmail.com>
2019-01-25 14:11:31 +0100

    actividades nuevas

---
+++ TEMA 8/Ejercicios/Practica3/Practica3_Ejercicio1/src/Clases/Productos.java
@@ -0,0 +1,41 @@
+
+package Clases;
+
+public class Productos {
+   private String nombre;
+   private int unidades;
+   private float precio;
+   private String nombreProveedor;/*sin terminar*/
+
+    public Productos(String nombre, int unidades, float precio) {
+        this.nombre = nombre;
+        this.unidades = unidades;
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getUnidades() {
+        return unidades;
+    }
+
+    public void setUnidades(int unidades) {
+        this.unidades = unidades;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+   
+   
+}
---
+++ TEMA 8/Ejercicios/Practica3/Practica3_Ejercicio1/src/practica3_ejercicio1/Practica3_Ejercicio1.java
@@ -0,0 +1,36 @@
+package practica3_ejercicio1;
+
+import Clases.Productos;
+import Ventanas.Almacen;
+import java.util.ArrayList;
+
+public class Practica3_Ejercicio1 {
+    private static ArrayList<Productos> listaProductos;
+    private static Productos p;
+    private static Almacen vA;
+
+    public static void main(String[] args) {
+        generarProductos();
+        vA = new Almacen();
+        vA.setVisible(true);
+    }
+    public static void finalizar(){
+        vA.dispose();
+        System.exit(0);
+    }
+
+    private static void generarProductos() {
+        listaProductos = new ArrayList<Productos>();
+        p = new Productos ("Producto1",10,3.5f);
+        listaProductos.add(p);
+        p = new Productos ("Producto",45,1.7f);
+        listaProductos.add(p);
+        p = new Productos ("Producto",64,5.5f);
+        listaProductos.add(p);
+        p = new Productos ("Producto",32,3.4f);
+        listaProductos.add(p);
+        p = new Productos ("Producto",12,7.8f);
+        listaProductos.add(p);
+    }
+    
+}
